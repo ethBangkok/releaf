ef5c50e55d30ee34dae98dac07277c4f5c43d0c3
Raghav Kattel <arg.ahv7@gmail.com>
2024-11-16 23:48:40 +0700

    Add treasury pool deposit functionality and refactor related components

--- app/utils/custom-contracts-calls.ts
+++ app/utils/custom-contracts-calls.ts
@@ -1,5 +1,8 @@
 import { deployedPoolContractAddress } from "@/constants/config";
-import { useReadFundPoolTreasurer } from "@/hooks/generated-contracts/fund-pool";
+import {
+  useReadFundPoolTreasurer,
+  useWriteFundPoolDepositFund,
+} from "@/hooks/generated-contracts/fund-pool";
 import { useAccount, useBalance } from "wagmi";
 
 export const useGetBalance = () => {
@@ -22,4 +25,22 @@ export const useCheckIfTreasurer = () => {
       },
     },
   });
+};
+
+// export const useCheckIfAdmin = () => {
+//   const account = useAccount();
+//   return useRead({
+//     address: deployedPoolContractAddress,
+//     args: [account?.address as `0x${string}`],
+//     query: {
+//       select(data) {
+//         return data || false;
+//       },
+//     },
+//   });
+// }
+
+export const useDepositTreasuryPool = (...args: any) => {
+  const account = useAccount();
+  return useWriteFundPoolDepositFund(...args);
 };
--- constants/config.ts
+++ constants/config.ts
@@ -1,11 +1,8 @@
-import { parseEther, parseGwei } from "viem";
+import { formatEther, parseEther, parseGwei } from "viem";
 
 export const deployedPoolContractAddress = (process.env
   .NEXT_PUBLIC_FUND_POOL_CONTRACT || "") as `0x${string}`;
 
 export const AMOUNT_TO_STAKE = parseGwei("1", "wei");
 
-export const AMOUNT_TO_STAKE_IN_ETH = parseEther(
-  String(AMOUNT_TO_STAKE),
-  "gwei"
-);
+export const AMOUNT_TO_STAKE_IN_ETH = formatEther(BigInt(AMOUNT_TO_STAKE));
