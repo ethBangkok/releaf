7b4de6f9597a546c14f07786a11f213a44496e38
Raghav Kattel <arg.ahv7@gmail.com>
2024-11-17 06:48:15 +0700

    Add disbursement functionality and new icon to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Bell, DollarSign, ExternalLink, Search, Users } from "lucide-react";
+import {
+  Bell,
+  DollarSign,
+  ExternalLink,
+  Search,
+  Users,
+  Users2,
+} from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -27,6 +34,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { useProjectTransactions } from "../utils/subgraph.query";
+import { useWriteFundPoolDistributeFunds } from "@/hooks/generated-contracts/fund-pool";
+import { deployedPoolContractAddress } from "@/constants/config";
 
 export default function Dashboard() {
   const { data } = useProjectTransactions();
@@ -52,30 +61,39 @@ export default function Dashboard() {
     }
   }, [data]);
 
+  const disbursement = useWriteFundPoolDistributeFunds();
+
+  const handleDisbursement = async () => {
+    console.log("Disbursing to beneficiaries");
+    await disbursement.writeContractAsync({
+      address: deployedPoolContractAddress,
+    });
+  };
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="flex items-center justify-between px-6 py-4 bg-white border-b">
-        <h1 className="text-2xl font-bold">Admin Dashboard</h1>
-        <div className="flex items-center space-x-4">
-          <Button variant="ghost" size="icon">
-            <Bell className="h-5 w-5" />
+    <div className='flex flex-col min-h-screen'>
+      <header className='flex items-center justify-between px-6 py-4 bg-white border-b'>
+        <h1 className='text-2xl font-bold'>Admin Dashboard</h1>
+        <div className='flex items-center space-x-4'>
+          <Button variant='ghost' size='icon'>
+            <Bell className='h-5 w-5' />
           </Button>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                <Avatar className="h-8 w-8">
-                  <AvatarImage src="/placeholder-avatar.jpg" alt="@johndoe" />
+              <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+                <Avatar className='h-8 w-8'>
+                  <AvatarImage src='/placeholder-avatar.jpg' alt='@johndoe' />
                   <AvatarFallback>JD</AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-              <DropdownMenuLabel className="font-normal">
-                <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">
+            <DropdownMenuContent className='w-56' align='end' forceMount>
+              <DropdownMenuLabel className='font-normal'>
+                <div className='flex flex-col space-y-1'>
+                  <p className='text-sm font-medium leading-none'>
                     wallet address
                   </p>
-                  <p className="text-xs leading-none text-muted-foreground">
+                  <p className='text-xs leading-none text-muted-foreground'>
                     test@example.com
                   </p>
                 </div>
@@ -86,58 +104,70 @@ export default function Dashboard() {
           </DropdownMenu>
         </div>
       </header>
-      <main className="flex-1 p-6 bg-gray-100">
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <main className='flex-1 p-6 bg-gray-100'>
+        <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium'>
                 Total Funding Pool Balance
               </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
+              <DollarSign className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">${totalBalance} MATIC</div>
+              <div className='text-2xl font-bold'>${totalBalance} MATIC</div>
             </CardContent>
           </Card>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium'>
                 Total Transactions
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{transactions.length}</div>
-              <p className="text-xs text-muted-foreground">
+              <div className='text-2xl font-bold'>{transactions.length}</div>
+              <p className='text-xs text-muted-foreground'>
                 All-time transactions
               </p>
             </CardContent>
           </Card>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium'>
                 Unique Wallets
               </CardTitle>
-              <Users className="h-4 w-4 text-muted-foreground" />
+              <Users className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">
+              <div className='text-2xl font-bold'>
                 {new Set([...transactions.map((t) => t.funder)]).size}
               </div>
-              <p className="text-xs text-muted-foreground">
+              <p className='text-xs text-muted-foreground'>
                 Interacted with the pool
               </p>
             </CardContent>
           </Card>
         </div>
-        <Card className="mt-6">
+        <Card className='mt-6'>
           <CardHeader>
-            <CardTitle>Transaction History</CardTitle>
+            <CardTitle>
+              <div className='flex justify-between items-center'>
+                <p>Transaction History</p>
+
+                <Button
+                  disabled={disbursement.isPending}
+                  onClick={handleDisbursement}
+                  variant={"outline"}>
+                  <Users2 />
+                  Disburse to Beneficiaries
+                </Button>
+              </div>
+            </CardTitle>
             <CardDescription>
               View all transactions in the funding pool
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="space-y-4">
+            <div className='space-y-4'>
               <Table>
                 <TableHeader>
                   <TableRow>
@@ -157,16 +187,15 @@ export default function Dashboard() {
                       <TableCell>{transaction.funder}</TableCell>
                       <TableCell>
                         <Button
-                          variant="outline"
-                          size="sm"
+                          variant='outline'
+                          size='sm'
                           onClick={() =>
                             window.open(
                               `https://eth-sepolia.blockscout.com/tx/${transaction.transactionHash}`,
                               "_blank"
                             )
-                          }
-                        >
-                          View <ExternalLink className="ml-2 h-4 w-4" />
+                          }>
+                          View <ExternalLink className='ml-2 h-4 w-4' />
                         </Button>
                       </TableCell>
                     </TableRow>
